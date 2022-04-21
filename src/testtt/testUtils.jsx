c76269932b8a13d16cfe4d2aa5e057083fc4595e
ummulmth <ummul.mth24@gmail.com>
2022-04-21 14:07:28 +0700

    trying to test utils

---
+++ src/testtt/testUtils.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { configureStore } from "@reduxjs/toolkit";
+import { ChakraProvider } from "@chakra-ui/react";
+import { Provider as ReduxProvider } from "react-redux";
+
+import playlistReducer from "../store/playlist";
+import authReducer from "../store/auth";
+
+const customRender = (
+	ui,
+	{
+		preloadedState,
+		store = configureStore({
+			reducer: { auth: authReducer, playlist: playlistReducer },
+			preloadedState,
+		}),
+		...renderOptions
+	} = {}
+) => {
+	const Providers = ({ children }) => (
+		<ReduxProvider store={store}>
+			<ChakraProvider>{children}</ChakraProvider>
+		</ReduxProvider>
+	);
+
+	return render(ui, { wrapper: Providers, ...renderOptions });
+};
+
+export * from "@testing-library/react";
+export { customRender as render };
