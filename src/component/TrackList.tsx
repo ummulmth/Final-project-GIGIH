1b6abcc650460ca57011c13eb049d8a0758ec846
ummulmth <ummul.mth24@gmail.com>
2022-04-21 13:24:26 +0700

    add track list

--- src/component/TrackList.tsx
+++ src/component/TrackList.tsx
@@ -1,83 +1,16 @@
 import { Box } from "@chakra-ui/react";
 import { useAppSelector } from "../store";
-import Track from "./Track";
+import Track from "./track";
 
 const TrackList = () => {
 	const { tracks } = useAppSelector((state) => state.playlist);
 
 	return (
-		<Box w={"100%"}></Box>
+		<Box w={"100%"}>
 			{tracks.map((track) => (
 				<Track track={track} key={track.id} />
 			))}
-		</MotionBox>
-	) : (
-		<Text>Search Your Music Here...</Text>
-	);
-};
-
-const Track = ({ track }: { track: SpotifyTrack }) => {
-	const dispatch = useAppDispatch();
-	const selectedTracks = useAppSelector(
-		(state) => state.playlist.selectedTracks
-	);
-
-	const isSelected: boolean = selectedTracks.includes(track.uri);
-
-	const handleSelect: React.MouseEventHandler<HTMLButtonElement> = () => {
-		if (isSelected) {
-			dispatch(substractSelectedTracks(track.uri));
-		} else {
-			dispatch(addSelectedTracks(track.uri));
-		}
-	};
-
-	const artists: JSX.Element[] = track.artists.map(
-		(artist: Artist, index: number) => {
-			const isLast: boolean = index === track.artists.length - 1;
-			return (
-				<a
-					href={artist?.external_urls?.spotify}
-					target="_blank"
-					rel="noreferrer"
-					key={artist.id}
-				>
-					{artist.name + (isLast ? "" : ", ")}
-				</a>
-			);
-		}
-	);
-
-	const image = track.album.images.find(
-		(image: SpotifyImage) => image.width === 64
-	);
-
-	const bg = useColorModeValue("white", "gray.800");
-	const shadow = useColorModeValue("md", "none");
-
-	return (
-		<Flex align="center" my={2} p={2} borderRadius="lg" _hover={{ bg, shadow }}>
-			<Image
-				w={"64px"}
-				h={"64px"}
-				borderRadius={2}
-				src={image?.url}
-				alt={track.name}
-			/>
-			<Flex mx={4} direction="column" flex={1}>
-				<Text fontWeight="bold">{track.name}</Text>
-				<Text>{artists}</Text>
-			</Flex>
-			<Flex>
-				<Button
-					onClick={handleSelect}
-					m={2}
-					variant={isSelected ? "ghost" : "solid"}
-				>
-					{isSelected ? "Deselect" : "Select"}
-				</Button>
-			</Flex>
-		</Flex>
+		</Box>
 	);
 };
 
